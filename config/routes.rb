e6f247fa00f2a6d785a60e0e31b5a1b34ae7d610
timcknowles <tim@Tims-MacBook-Air.local>
2016-09-28 20:46:24 +0100

    follow up working

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'patients#index'
 end
